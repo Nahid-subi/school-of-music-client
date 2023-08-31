239123b8f5c8a0f9ff333061a34a79bef30716b5
Nahid-subi <scientisthassannahid@gmail.com>
2023-08-31 10:09:59 +0600

    routes set up done

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+
+const Home = () => {
+    return (
+        <div className="bg-blue">
+            <h3 className="text-yellow-300">this is home</h3>
+            <h1 className="text-header">Welcome to HarmonyNotes Music School</h1>
+            <p className="text-body">
+                Our passionate instructors are dedicated to nurturing your musical journey.
+            </p>
+            <button className="btn btn-yellow">Learn More</button>
+        </div>
+    );
+};
+
+export default Home;
