d47cde3218c8d12b4514482f5d2384f58d2cdc08
Nahid-subi <scientisthassannahid@gmail.com>
2023-09-02 16:05:08 +0600

    popular instructors added

---
+++ src/Pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import InstructorCard from "../../../components/InstructorCard/InstructorCard";
+
+const PopularInstructors = () => {
+    const [classes, setClasses] = useState([]);
+
+    useEffect(() => {
+        fetch('classes.json')
+            .then(res => res.json())
+            .then(data => setClasses(data.slice(0, 6))) // Limit to 6 items
+    }, []);
+    return (
+        <div>
+            <SectionTitle heading="Popular Instructors"></SectionTitle>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {classes.map(classItem => (
+                    <InstructorCard 
+                    classItem={classItem}
+                    key={classItem._id}>
+                    </InstructorCard>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default PopularInstructors;
--- src/components/InstructorCard/InstructorCard.jsx
+++ src/components/InstructorCard/InstructorCard.jsx
@@ -2,9 +2,9 @@
 const InstructorCard = ({classItem}) => {
     console.log(classItem)
     return (
-        <div>
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure><img src={classItem.imageInstructor} /></figure>
+        <div className="flex justify-center mx-2">
+            <div className="card card-compact w-60 bg-base-100 shadow-xl">
+                <figure><img className="w-full h-44" src={classItem.imageInstructor} /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{classItem.instructorName}</h2>
                     <h3 className="text-blue text-base font-semibold">Email: {classItem.instructorEmail}</h3>
